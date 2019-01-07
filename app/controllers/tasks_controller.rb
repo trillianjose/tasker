6f65c90369cb15c1929678117125b1d7f9d78c9f
trillianjose <trillianjosea@gmail.com>
2019-01-07 17:39:52 +0100

    added show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,8 @@ class TasksController < ApplicationController
   def index
     @task = Task.order(:created_at)
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
   root 'tasks#index'
+  get 'tasks/:id' => 'tasks#show', as: 'task'
 end
