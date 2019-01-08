f3602c8dc75b7dde15dc6821ee1af82bd1eb2f11
trillianjose <trillianjosea@gmail.com>
2019-01-08 14:41:34 +0100

    fixing pull request errors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,23 @@
 class TasksController < ApplicationController
-
   def index
     @task = Task.order(:created_at)
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(:description)
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
   root 'tasks#index'
-  get 'tasks/:id' => 'tasks#show', as: 'task'
+
+  resources :tasks
 end
